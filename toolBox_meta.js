a5306806da062e4a1c01bd5520c0902f44586eda
cirept <cirept612@gmail.com>
2017-02-08 12:30:51 -0800

    asd

---
+++ toolBox_meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name QA Toolbox
+// @namespace www.cobaltgroup.com/
+// @include http:*
+// @version 2.7.2
+// @downloadURL http://media-dmg.assets-cdk.com/teams/repository/export/460/264509aae1005845e0050568ba825/460264509aae1005845e0050568ba825.js
+// @run-at document-end
+// @description Makes life easier... I hope.
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.js
+// @require https://use.fontawesome.com/3953f47d82.js
+// @require https://rawgit.com/cirept/Tetra/master/edoToolBox.js
+// @resource https://fonts.googleapis.com/css?family=Montserrat
+// @author Eric Tanaka
+// @grant GM_openInTab
+// @grant GM_setClipboard
+// @grant unsafeWindow
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_info
+// @noframes
+// ==/UserScript==
+
+
+// @require http://media-dmg.assets-cdk.com/teams/repository/export/31f/d23a89e25100589270050568b6442/31fd23a89e25100589270050568b6442.js
