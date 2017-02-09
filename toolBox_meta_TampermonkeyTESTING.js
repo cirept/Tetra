45a96952bb42de4c63b00eea4a36208b15bae18f
cirept <cirept612@gmail.com>
2017-02-08 16:19:47 -0800

    865tri

--- toolBox_meta_TampermonkeyTESTING.js
+++ toolBox_meta_TampermonkeyTESTING.js
@@ -8,7 +8,7 @@
 // @description Makes life easier... I hope.
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.js
 // @require https://use.fontawesome.com/3953f47d82.js
-// @require https://rawgit.com/cirept/Tetra/master/toolBox_tampermonkeyTESTING.js
+// @require https://cdn.rawgit.com/cirept/Tetra/2.7.4/toolBox_tampermonkeyTESTING.js
 // @resource https://fonts.googleapis.com/css?family=Montserrat
 // @author Eric Tanaka
 // @grant GM_openInTab
