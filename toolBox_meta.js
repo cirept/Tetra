f8dd5de853aa068c3a6d96f443c4941064f0b961
cirept <cirept612@gmail.com>
2017-02-09 14:05:35 -0800

    modified meta

--- toolBox_meta.js
+++ toolBox_meta.js
@@ -2,12 +2,13 @@
 // @name QA Toolbox
 // @namespace www.cobaltgroup.com/
 // @include http:*
-// @version 2.7.6
-// @downloadURL https://rawgit.com/cirept/Tetra/master/toolBox_meta.js
+// @version 2.7.5
+// @downloadURL https://cdn.rawgit.com/cirept/Tetra/master/toolBox_meta.js
 // @run-at document-end
 // @description Makes life easier... I hope.
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://use.fontawesome.com/3953f47d82.js
+// @require https://cdn.rawgit.com/cirept/Tetra/2.7.6/toolBox.js
 // @resource https://fonts.googleapis.com/css?family=Montserrat
 // @author Eric Tanaka
 // @grant GM_openInTab
