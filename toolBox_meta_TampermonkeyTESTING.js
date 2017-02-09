7e0ddc43331bedd8ce5790ed3a399a8fbcd9c0c3
cirept <cirept612@gmail.com>
2017-02-08 16:12:17 -0800

    TESTING FILES

---
+++ toolBox_meta_TampermonkeyTESTING.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name QA Toolbox
+// @namespace www.cobaltgroup.com/
+// @include http:*
+// @version 2.7.3
+// @updateURL https://rawgit.com/cirept/Tetra/master/toolBox_meta.js
+// @run-at document-end
+// @description Makes life easier... I hope.
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.js
+// @require https://use.fontawesome.com/3953f47d82.js
+// @require https://rawgit.com/cirept/Tetra/master/toolBox.js
+// @resource https://fonts.googleapis.com/css?family=Montserrat
+// @author Eric Tanaka
+// @grant GM_openInTab
+// @grant GM_setClipboard
+// @grant unsafeWindow
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_info
+// @noframes
+// ==/UserScript==
