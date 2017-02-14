27a1cf9db0a8bb94805200aeb6fa4ac4ae831298
cirept <cirept612@gmail.com>
2017-02-14 07:24:33 -0800

    misc edits
    nothing changed

--- toolBox_meta.js
+++ toolBox_meta.js
@@ -25,29 +25,40 @@
 // 2.7.7 updates
 /* ----------------------------------------
 - TOOLBAR LOOK UPDATED
+
 ---- Separated out tools into individual panel sections
 ---- Added minimize button for entire toolbar
 ---- Added collapsable panel sections (click the panel title to minimize section)
 ---- Added version number at the bottom to track changes
 ---- Legend look has been updated
 ---- Added UI effects
+
 - NEW TOOLS ADDED
+
 ---- 404 Checker Button
 -------- Will test all links on the page to verify that it works
+
 ---- View mobile site Button
 -------- Will open a new tab with the mobile site
+
 ---- Refresh button Toggle
 -------- Will refresh the site
+
 ---- Hide Preview Toolbar Toggle (nextGen Sites only)
 -------- Will hide the CDK Global PCE toolbar at the top of the site
+
 ---- NextGen Parameter toggle
 -------- Will force the nextGen site to show, if available.  Will cause site error if nextGen option for page is not available
+
 ---- m4 Parameters toggle
 -------- Will apply "&comments=true&relative=true" to current URL
+
 ---- show autofill tags toggles
 -------- Will show all autofill tags on page
+
 ---- force desktop site toggle (TETRA)
 -------- Will apply "device=immobile" to force desktop site
+
 ---- Page informaiton
 -------- Added more page details
 ---------------------------------------- */
